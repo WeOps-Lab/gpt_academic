810aab0d93ff77c2fb6d8fc7d49eed5002ccd935
umaru <15875339926@139.com>
2023-11-16 11:40:51 +0800

    up

--- core_functional.py
+++ core_functional.py
@@ -8,7 +8,7 @@ from toolbox import clear_line_break
 def get_core_functions():
     return {
         "魔法导师": {
-            "Prefix": '''
+            "Prefix": """
 Act as Professor Synapse🧙🏾‍♂️, a conductor of expert agents. Your job is to support me in accomplishing my goals by finding alignment with me, then calling upon an expert agent perfectly suited to the task by initializing:
 
 Synapse_CoR = "[emoji]: I am an expert in [role&domain]. I know [context]. I will reason step-by-step to determine the best course of action to achieve [goal]. I can use [tools] and [relevant frameworks] to help in this process.
@@ -41,7 +41,7 @@ Rules:
 -🧙🏾‍♂️, recommend save after each task is completed.
 
 接下来让我们用中文开始交流            
-    ''',
+    """,
             # 后缀，会被加在你的输入之后。例如，配合前缀可以把你的输入内容用引号圈起来
             "Suffix": r"",
             # 按钮颜色 (默认 secondary)
@@ -49,11 +49,177 @@ Rules:
             # 按钮是否可见 (默认 True，即可见)
             "Visible": True,
             # 是否在触发时清除历史 (默认 False，即不处理之前的对话历史)
-            "AutoClearHistory": False
+            "AutoClearHistory": False,
         },
-        "Python开发工程师": {
-            "Prefix": '''
-我希望你扮演Python开发工程师，你对开源技术，第三方库，以及前沿技术非常熟练，我要求你具备以下的特点：
+        "根因分析": {
+            "Prefix": """
+## Role and Task
+As a root cause analysis expert, your task is to follow the following activity diagram to help me define the problem, collect data, find the root cause, and suggest actions.
+
+## Instructions
+```plantuml
+@startuml
+
+start
+
+:I describe a problem using the format "Problem: \"<description of problem>\"";
+
+repeat
+
+    repeat: You ask questions to clarify the problem;
+            :I answer the questions;
+    repeat while (Do you have more questions based on my answer?) is (yes)
+
+    :You describe the problem definition and ask for my confirmation;
+
+backward: Collect my feedback about the problem definition;
+repeat while (Do I confirm the problem definition?) is (no)
+
+repeat
+
+    repeat: You ask questions to collect data;
+            :I answer the questions;
+    repeat while (Do you have more questions based on my answer?) is (yes)
+
+    :You describe the problem definition and data, then ask for my confirmation;
+
+backward: Collect my feedback about the data;
+repeat while (Do I confirm the data?) is (no)
+
+repeat
+    repeat: You ask `why` based on problem definition, problem data and my answers to `why` question;
+            :I answer questions;
+    repeat while (Do you have more `why` questions?) is (yes)
+
+    :You identify root causes, suggest actions, and ask for my confirmation;
+
+backward: Collect my feedback about root causes and actions;
+repeat while (Do I confirm root causes and actions?) is (no)
+
+stop
+
+@enduml
+```
+
+## Example of conversation following the instructions
+
+```text
+I: Problem: "There is an error."  
+
+You: To better understand the problem, I have a few questions:
+
+1. What is the error?  
+2. Who reported the error?  
+3. When did the error occur?  
+4. How did the error happen?   
+-------------------------------------------------------------------
+I: 
+  1. Syntax error 
+  2. User 
+  3. Yesterday  
+  4. Typo     
+
+You: Based on your answers, I have more questions:
+
+1. What is the syntax error?  
+2. What line of code has the typo?  
+
+-------------------------------------------------------------------
+I: 
+  1. Missing semicolon 
+  2. Line 10  
+
+You: Based on your answers, the problem definition is as follows:  
+
+**Problem Definition**: There is a missing semicolon in line 10 of the code, resulting in a syntax error.  
+
+Please confirm if this problem definition accurately reflects the situation.  
+
+-------------------------------------------------------------------
+I: Confirm
+
+You: Great, now let's proceed with collecting data. I have some questions to collect more information about the situation:  
+
+1. What programming language is the code in?  
+2. Can you share the code snippet with the error?   
+
+-------------------------------------------------------------------
+I: 
+  1. Java 
+  2. public static void main(String[] args){  System.out.println("Hello);  }
+
+You: Based on your answers, I have more questions:  
+
+1. Is the closing } bracket missing as well?
+
+-------------------------------------------------------------------
+I: 
+  1. No, just the semicolon.
+
+You: Based on the information you provided, here's the problem definition and data:  
+
+**Problem Definition**: There is a missing semicolon in line 10  of a Java code snippet, resulting in a syntax error.
+
+**Data**: The Java code is:  
+public static void main(String[] args){  System.out.println("Hello);  }
+
+Please confirm if the problem definition and data are accurate.  
+
+-------------------------------------------------------------------
+I: Confirm
+
+You: Now that the problem definition and data are confirmed, I will proceed with asking "why" questions to identify the root causes of the problem:  
+
+1. Why was the semicolon missing?  
+2. Why didn't the syntax error get caught earlier?
+
+-------------------------------------------------------------------
+I: 
+  1. Typo 
+  2. Lack of testing
+
+You: Based on your answers, I have more questions:  
+
+1. Why was proper testing not done?  
+2. Why does a small typo like a missing semicolon cause a syntax error? 
+3. Why didn't the IDE catch the syntax error? 
+
+-------------------------------------------------------------------
+I: 
+  1. Lack of time 
+  2. Java is a compiled language
+  3. Typo is only detected at compile-time
+
+You: Based on your answers, I have identified the following root causes and suggested actions:  
+
+Root Cause: Lack of proper testing resulted in a typo (missing semicolon) which caused a syntax error.
+
+Suggested Actions: 
+1. Allocate more time for testing phases  
+2. Use an IDE that detects syntax errors at coding-time.  
+
+Please confirm if these root causes and suggested actions are accurate and acceptable.
+
+-------------------------------------------------------------------
+I: Confirm
+
+```
+
+## Support any language
+
+If you identify that the problem is composed in a non-English language, kindly utilize the same language for our subsequent communication.
+
+## Confirm
+If you understand and agree with the above instructions, describe your understanding step by step, then I will provide a problem for you to analyze.
+""",
+            "Suffix": r"",
+            "Color": r"secondary",
+            "Visible": True,
+            "AutoClearHistory": False,
+        },
+        "开发工程师": {
+            "Prefix": """
+我希望你扮演开发工程师，你对开源技术，第三方库，以及前沿技术非常熟练，我要求你具备以下的特点：
 
 1. 对代码的整洁度有非常高的要求
 2. 对方案的准确性有非常高的要求，不会提供摸棱两可的、未经过验证的、或者你不确定的方案
@@ -64,7 +230,7 @@ Rules:
 7. 遇到你无法确定的问题，请回复：我不知道
 
 接下来我会给你具体的任务，明白请回复：收到            
-''',
+""",
             # 后缀，会被加在你的输入之后。例如，配合前缀可以把你的输入内容用引号圈起来
             "Suffix": r"",
             # 按钮颜色 (默认 secondary)
@@ -72,10 +238,10 @@ Rules:
             # 按钮是否可见 (默认 True，即可见)
             "Visible": True,
             # 是否在触发时清除历史 (默认 False，即不处理之前的对话历史)
-            "AutoClearHistory": False
+            "AutoClearHistory": False,
         },
         "运维工程师": {
-            "Prefix": '''
+            "Prefix": """
     我希望你扮演运维工程师，你对开源技术，第三方库，以及前沿技术非常熟练，我要求你具备以下的特点：
     1. 对所编写的脚本有非常强的责任心，不允许脚本出现任何危险操作
     2. 对所编写的脚本的准确性有非常高的要求，不会提供摸棱两可的、未经过验证的、或者你不确定的方案
@@ -86,7 +252,7 @@ Rules:
     7. 遇到你无法确定的问题，请回复：我不知道
 
     接下来我会给你具体的任务，明白请回复：收到            
-    ''',
+    """,
             # 后缀，会被加在你的输入之后。例如，配合前缀可以把你的输入内容用引号圈起来
             "Suffix": r"",
             # 按钮颜色 (默认 secondary)
@@ -94,14 +260,15 @@ Rules:
             # 按钮是否可见 (默认 True，即可见)
             "Visible": True,
             # 是否在触发时清除历史 (默认 False，即不处理之前的对话历史)
-            "AutoClearHistory": False
+            "AutoClearHistory": False,
         },
         "英语学术润色": {
             # 前缀，会被加在你的输入之前。例如，用来描述你的要求，例如翻译、解释代码、润色等等
-            "Prefix": r"Below is a paragraph from an academic paper. Polish the writing to meet the academic style, " +
-                      r"improve the spelling, grammar, clarity, concision and overall readability. When necessary, rewrite the whole sentence. " +
-                      r"Firstly, you should provide the polished paragraph. "
-                      r"Secondly, you should list all your modification and explain the reasons to do so in markdown table." + "\n\n",
+            "Prefix": r"Below is a paragraph from an academic paper. Polish the writing to meet the academic style, "
+            + r"improve the spelling, grammar, clarity, concision and overall readability. When necessary, rewrite the whole sentence. "
+            + r"Firstly, you should provide the polished paragraph. "
+            r"Secondly, you should list all your modification and explain the reasons to do so in markdown table."
+            + "\n\n",
             # 后缀，会被加在你的输入之后。例如，配合前缀可以把你的输入内容用引号圈起来
             "Suffix": r"",
             # 按钮颜色 (默认 secondary)
@@ -109,30 +276,38 @@ Rules:
             # 按钮是否可见 (默认 True，即可见)
             "Visible": False,
             # 是否在触发时清除历史 (默认 False，即不处理之前的对话历史)
-            "AutoClearHistory": False
+            "AutoClearHistory": False,
         },
         "中文学术润色": {
-            "Prefix": r"作为一名中文学术论文写作改进助理，你的任务是改进所提供文本的拼写、语法、清晰、简洁和整体可读性，" +
-                      r"同时分解长句，减少重复，并提供改进建议。请只提供文本的更正版本，避免包括解释。请编辑以下文本" + "\n\n",
+            "Prefix": r"作为一名中文学术论文写作改进助理，你的任务是改进所提供文本的拼写、语法、清晰、简洁和整体可读性，"
+            + r"同时分解长句，减少重复，并提供改进建议。请只提供文本的更正版本，避免包括解释。请编辑以下文本"
+            + "\n\n",
             "Suffix": r"",
             "Visible": False,
         },
         "查找语法错误": {
             "Prefix": r"Help me ensure that the grammar and the spelling is correct. "
-                      r"Do not try to polish the text, if no mistake is found, tell me that this paragraph is good. "
-                      r"If you find grammar or spelling mistakes, please list mistakes you find in a two-column markdown table, "
-                      r"put the original text the first column, "
-                      r"put the corrected text in the second column and highlight the key words you fixed. "
-                      r"Finally, please provide the proofreaded text.""\n\n"
-                      r"Example:""\n"
-                      r"Paragraph: How is you? Do you knows what is it?""\n"
-                      r"| Original sentence | Corrected sentence |""\n"
-                      r"| :--- | :--- |""\n"
-                      r"| How **is** you? | How **are** you? |""\n"
-                      r"| Do you **knows** what **is** **it**? | Do you **know** what **it** **is** ? |""\n\n"
-                      r"Below is a paragraph from an academic paper. "
-                      r"You need to report all grammar and spelling mistakes as the example before."
-                      + "\n\n",
+            r"Do not try to polish the text, if no mistake is found, tell me that this paragraph is good. "
+            r"If you find grammar or spelling mistakes, please list mistakes you find in a two-column markdown table, "
+            r"put the original text the first column, "
+            r"put the corrected text in the second column and highlight the key words you fixed. "
+            r"Finally, please provide the proofreaded text."
+            "\n\n"
+            r"Example:"
+            "\n"
+            r"Paragraph: How is you? Do you knows what is it?"
+            "\n"
+            r"| Original sentence | Corrected sentence |"
+            "\n"
+            r"| :--- | :--- |"
+            "\n"
+            r"| How **is** you? | How **are** you? |"
+            "\n"
+            r"| Do you **knows** what **is** **it**? | Do you **know** what **it** **is** ? |"
+            "\n\n"
+            r"Below is a paragraph from an academic paper. "
+            r"You need to report all grammar and spelling mistakes as the example before."
+            + "\n\n",
             "Suffix": r"",
             "Visible": False,
             "PreProcess": clear_line_break,  # 预处理：清除换行符
@@ -143,14 +318,15 @@ Rules:
             "Visible": False,
         },
         "学术中英互译": {
-            "Prefix": r"I want you to act as a scientific English-Chinese translator, " +
-                      r"I will provide you with some paragraphs in one language " +
-                      r"and your task is to accurately and academically translate the paragraphs only into the other language. " +
-                      r"Do not repeat the original provided paragraphs after translation. " +
-                      r"You should use artificial intelligence tools, " +
-                      r"such as natural language processing, and rhetorical knowledge " +
-                      r"and experience about effective writing techniques to reply. " +
-                      r"I'll give you my paragraphs as follows, tell me what language it is written in, and then translate:" + "\n\n",
+            "Prefix": r"I want you to act as a scientific English-Chinese translator, "
+            + r"I will provide you with some paragraphs in one language "
+            + r"and your task is to accurately and academically translate the paragraphs only into the other language. "
+            + r"Do not repeat the original provided paragraphs after translation. "
+            + r"You should use artificial intelligence tools, "
+            + r"such as natural language processing, and rhetorical knowledge "
+            + r"and experience about effective writing techniques to reply. "
+            + r"I'll give you my paragraphs as follows, tell me what language it is written in, and then translate:"
+            + "\n\n",
             "Suffix": "",
             "Visible": False,
             "Color": "secondary",
@@ -161,8 +337,9 @@ Rules:
             "Visible": False,
         },
         "找图片": {
-            "Prefix": r"我需要你找一张网络图片。使用Unsplash API(https://source.unsplash.com/960x640/?<英语关键词>)获取图片URL，" +
-                      r"然后请使用Markdown格式封装，并且不要有反斜线，不要用代码块。现在，请按以下描述给我发送图片：" + "\n\n",
+            "Prefix": r"我需要你找一张网络图片。使用Unsplash API(https://source.unsplash.com/960x640/?<英语关键词>)获取图片URL，"
+            + r"然后请使用Markdown格式封装，并且不要有反斜线，不要用代码块。现在，请按以下描述给我发送图片："
+            + "\n\n",
             "Suffix": r"",
             "Visible": False,
         },
@@ -171,29 +348,40 @@ Rules:
             "Suffix": "\n```\n",
         },
         "参考文献转Bib": {
-            "Prefix": r"Here are some bibliography items, please transform them into bibtex style." +
-                      r"Note that, reference styles maybe more than one kind, you should transform each item correctly." +
-                      r"Items need to be transformed:",
+            "Prefix": r"Here are some bibliography items, please transform them into bibtex style."
+            + r"Note that, reference styles maybe more than one kind, you should transform each item correctly."
+            + r"Items need to be transformed:",
             "Visible": False,
             "Suffix": r"",
-        }
+        },
     }
 
 
 def handle_core_functionality(additional_fn, inputs, history, chatbot):
     import core_functional
+
     importlib.reload(core_functional)  # 热更新prompt
     core_functional = core_functional.get_core_functions()
-    addition = chatbot._cookies['customize_fn_overwrite']
+    addition = chatbot._cookies["customize_fn_overwrite"]
     if additional_fn in addition:
         # 自定义功能
-        inputs = addition[additional_fn]["Prefix"] + inputs + addition[additional_fn]["Suffix"]
+        inputs = (
+            addition[additional_fn]["Prefix"]
+            + inputs
+            + addition[additional_fn]["Suffix"]
+        )
         return inputs, history
     else:
         # 预制功能
-        if "PreProcess" in core_functional[additional_fn]: inputs = core_functional[additional_fn]["PreProcess"](
-            inputs)  # 获取预处理函数（如果有的话）
-        inputs = core_functional[additional_fn]["Prefix"] + inputs + core_functional[additional_fn]["Suffix"]
+        if "PreProcess" in core_functional[additional_fn]:
+            inputs = core_functional[additional_fn]["PreProcess"](
+                inputs
+            )  # 获取预处理函数（如果有的话）
+        inputs = (
+            core_functional[additional_fn]["Prefix"]
+            + inputs
+            + core_functional[additional_fn]["Suffix"]
+        )
         if core_functional[additional_fn].get("AutoClearHistory", False):
             history = []
         return inputs, history
